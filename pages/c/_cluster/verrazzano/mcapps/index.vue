760c11372542946a905a54b0fbe730b2d8ace91c
Richard Killen <36244611+rakillen@users.noreply.github.com>
2022-08-15 14:23:57 -0500

    Added multi-cluster applications to Verrazzano menu (#31)
    * Added multi-cluster applications to Verrazzano menu
    
    * Make multi-cluster apps menu default

---
+++ pages/c/_cluster/verrazzano/mcapps/index.vue
@@ -0,0 +1,16 @@
+<script>
+// Added by Verrazzano
+import ResourceList from '@/components/ResourceList';
+
+export default {
+  name:       'MultiClusterAppsResourceList',
+  components: { ResourceList },
+  created() {
+    this.$route.params['resource'] = 'clusters.verrazzano.io.MultiClusterApplicationConfiguration';
+  }
+};
+</script>
+
+<template>
+  <ResourceList />
+</template>
