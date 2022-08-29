e56b406f145e9ad816378d92ba0bf5adc0b7d696
Richard Killen <richard.killen@oracle.com>
2022-08-29 14:31:23 -0500

    Avoid 404 on reload of component page

--- pkg/verrazzano/pages/c/_cluster/components/index.vue
+++ pkg/verrazzano/pages/c/_cluster/components/index.vue
@@ -5,6 +5,9 @@ import ResourceList from '@shell/components/ResourceList';
 export default {
   name:       'ComponentsResourceList',
   components: { ResourceList },
+  created() {
+    this.$route.params['resource'] = 'core.oam.dev.component';
+  }
 };
 </script>
 
