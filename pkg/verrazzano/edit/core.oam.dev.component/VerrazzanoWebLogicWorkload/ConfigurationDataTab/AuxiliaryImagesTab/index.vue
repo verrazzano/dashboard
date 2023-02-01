51736e974aff430acd21bbbb3f437f718a6cc0f9
Mason Taube <mason.taube@oracle.com>
2023-02-01 11:51:11 -0500

    break out volume settings from Auxiliary Images tab, assign weights to OnlineUpdatesTab and AuxiliaryImagesTab

--- pkg/verrazzano/edit/core.oam.dev.component/VerrazzanoWebLogicWorkload/ConfigurationDataTab/AuxiliaryImagesTab/index.vue
+++ pkg/verrazzano/edit/core.oam.dev.component/VerrazzanoWebLogicWorkload/ConfigurationDataTab/AuxiliaryImagesTab/index.vue
@@ -64,8 +64,10 @@ export default {
       set(neu) {
         this.set(this.namespacedObject, 'spec.workload.spec.template.spec.configuration.model.auxiliaryImageVolumeSizeLimit', neu );
       },
-
     },
+    advancedTabLabel() {
+      return this.t('verrazzano.weblogic.tabs.auxiliaryImageSettings');
+    }
   },
   data() {
     return {
@@ -82,7 +84,7 @@ export default {
 </script>
 
 <template>
-  <TreeTab :name="treeTabName" :label="treeTabLabel">
+  <TreeTab :name="treeTabName" :label="treeTabLabel" :weight="weight">
     <template #beside-header>
       <TabDeleteButton
         :element-name="treeTabLabel"
@@ -90,40 +92,44 @@ export default {
         @click="dynamicListClearChildrenList"
       />
     </template>
-    <template #default>
-      <div>
-        <div class="row">
-          <div class="col span-2">
-            <LabeledInput
-              v-model="auxiliaryImageVolumeMountPath"
-              :mode="mode"
-              :placeholder="getNotSetPlaceholder(namespacedObject,'verrazzano.common.fields.auxiliaryImageVolumeMountPath')"
-              :label="t('verrazzano.common.fields.auxiliaryImageVolumeMountPath')"
-            />
-          </div>
-          <div class="col span-2">
-            <!-- auxiliaryImageVolumeMedium -->
-            <LabeledInput
-              v-model="auxiliaryImageVolumeMedium"
-              type="Number"
-              min="0"
-              :mode="mode"
-              :placeholder="getNotSetPlaceholder(namespacedObject,'verrazzano.common.fields.auxiliaryImageVolumeMedium')"
-              :label="t('verrazzano.common.fields.auxiliaryImageVolumeMedium')"
-            />
-          </div>
-          <div class="col span-2">
-            <LabeledInput
-              v-model="auxiliaryImageVolumeSizeLimit"
-              type="Number"
-              min="0"
-              :mode="mode"
-              :placeholder="getNotSetPlaceholder(namespacedObject,'verrazzano.common.fields.auxiliaryImageVolumeSizeLimit')"
-              :label="t('verrazzano.common.fields.auxiliaryImageVolumeSizeLimit')"
-            />
+    <template #nestedTabs>
+      <TreeTab name="advanced" :label="advancedTabLabel">
+        <div>
+          <div class="row">
+            <div class="col span-2">
+              <LabeledInput
+                v-model="auxiliaryImageVolumeMountPath"
+                :mode="mode"
+                :placeholder="getNotSetPlaceholder(namespacedObject,'verrazzano.common.fields.auxiliaryImageVolumeMountPath')"
+                :label="t('verrazzano.common.fields.auxiliaryImageVolumeMountPath')"
+              />
+            </div>
+            <div class="col span-2">
+              <!-- auxiliaryImageVolumeMedium -->
+              <LabeledInput
+                v-model="auxiliaryImageVolumeMedium"
+                type="Number"
+                min="0"
+                :mode="mode"
+                :placeholder="getNotSetPlaceholder(namespacedObject,'verrazzano.common.fields.auxiliaryImageVolumeMedium')"
+                :label="t('verrazzano.common.fields.auxiliaryImageVolumeMedium')"
+              />
+            </div>
+            <div class="col span-2">
+              <LabeledInput
+                v-model="auxiliaryImageVolumeSizeLimit"
+                type="Number"
+                min="0"
+                :mode="mode"
+                :placeholder="getNotSetPlaceholder(namespacedObject,'verrazzano.common.fields.auxiliaryImageVolumeSizeLimit')"
+                :label="t('verrazzano.common.fields.auxiliaryImageVolumeSizeLimit')"
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </TreeTab>
+    </template>
+    <template #default>
       <div class="spacer-small" />
       <div class="row">
         <div class="col span-8">
